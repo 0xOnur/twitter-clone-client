07336cf64818f2565fbf90a419b1d92e6c429381
onur <onurabat3@gmail.com>
2023-04-01 04:27:43 +0300

    improvement: added media support
    A maximum of 4 images/gif can be uploaded.
    A maximum of 1 video can be uploaded.
    Video and picture/gif cannot be uploaded at the same time.

--- src/components/middleSectionComp/TweetComposer.tsx
+++ src/components/middleSectionComp/TweetComposer.tsx
@@ -1,10 +1,11 @@
-import React, { useState, useRef, useCallback, ChangeEvent } from "react";
+import React, { useState, useRef, useCallback } from "react";
 import {
   ImageIcon,
   GIFIcon,
   PollIcon,
   ScheduleIcon,
   AddThreadIcon,
+  RemoveItemIcon,
 } from "@icons/Icon";
 import { Composer } from "@components/index";
 import { MiddleSection } from "@components/index";
@@ -17,6 +18,9 @@ const TweetComposer = () => {
   const [whoCanReply, setCanReply] = useState("Everyone");
 
   const [tweet, setTweet] = useState("");
+  const [mediaURLs, setMediaURLs] = useState<{ url: string; type: "image" | "video" }[]>([]);
+
+  console.log(mediaURLs)
 
   const tweetSettings = {
     visible: Audience,
@@ -26,7 +30,7 @@ const TweetComposer = () => {
   const textAreaRef = useRef<HTMLTextAreaElement>(null);
   Composer.useAutosizeTextArea(textAreaRef, tweet);
 
-  const handleChange = useCallback(
+  const handleTweetChange = useCallback(
     (evt: React.ChangeEvent<HTMLTextAreaElement>) => {
       const value = evt.target?.value;
       setTweet(value);
@@ -34,23 +38,53 @@ const TweetComposer = () => {
     []
   );
 
-  const handleFileUpload = useCallback(
-    (event: ChangeEvent<HTMLInputElement>) => {
-      const selectedFile = event.target.files?.[0];
-      if (selectedFile) {
-        console.log(selectedFile);
+  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { files } = event.target;
+  
+    if (!files) return;
+  
+    const mediaFiles = Array.from(files).map((file) => ({
+      url: URL.createObjectURL(file),
+      type: file.type.startsWith("image") ? "image" : "video",
+    }));
+  
+    setMediaURLs((prevMediaURLs: any) => {
+      const imagesCount = prevMediaURLs.filter((media: any) => media.type === "image").length;
+      const videoCount = prevMediaURLs.filter((media: any) => media.type === "video").length;
+  
+      const newImages = mediaFiles.filter((media) => media.type === "image");
+      const newVideo = mediaFiles.find((media) => media.type === "video");
+  
+      const maxImagesAllowed = 4 - imagesCount;
+      const imagesToAdd = newImages.slice(0, maxImagesAllowed);
+  
+      if (videoCount === 0 && imagesCount === 0 && newVideo) {
+        return [newVideo];
       }
-    },
-    []
-  );
+  
+      if (videoCount === 0 && newImages.length > 0) {
+        return [...prevMediaURLs, ...imagesToAdd];
+      }
+  
+      return prevMediaURLs;
+    });
+  };
+  
+  
+  
+  const handleDeleteImage = (indexToDelete: number) => {
+    setMediaURLs((prevmediaURLs) =>
+      prevmediaURLs.filter((_, index) => index !== indexToDelete)
+    );
+  };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log({
       tweet,
       tweetSettings,
-    })
-  }
+    });
+  };
 
   return (
     <div>
@@ -68,15 +102,14 @@ const TweetComposer = () => {
             </div>
             <div className="flex flex-col w-full pt-1">
               <form onSubmit={handleSubmit} className="flex flex-col">
-                {isWritingTweet || tweet.length>0 ? (
+                {isWritingTweet || tweet.length > 0 || mediaURLs.length > 0 ? (
                   <Composer.ChooseAudience
                     Audience={Audience}
                     setAudience={setAudience}
                   />
-                ): (null)}
+                ) : null}
 
                 <div className="relative flex-grow">
-                  
                   <div className="py-3">
                     {/* create textarea for tweet */}
                     <textarea
@@ -87,42 +120,99 @@ const TweetComposer = () => {
                       }}
                       ref={textAreaRef}
                       value={tweet}
-                      onChange={handleChange}
+                      onChange={handleTweetChange}
                       maxLength={280}
                     />
                   </div>
                 </div>
-                {isWritingTweet || tweet.length>0 ? (
+
+
+                <div
+                    className={`grid gap-2 mb-1 ${
+                      mediaURLs.length === 1 || (mediaURLs.length === 2 && mediaURLs[0].type === "video")
+                        ? "grid-cols-1"
+                        : "grid-cols-2"
+                    }`}
+                  >
+                  {mediaURLs.map((media, index) => (
+                    <div key={index} className="relative">
+                      {media.type === "image" ? (
+                        <img
+                          src={media.url}
+                          alt={`Selected file ${index + 1}`}
+                          className={`w-full rounded-xl object-cover ${
+                            mediaURLs.length > 1 ? "h-36" : "h-full"
+                          }`}
+                        />
+                      ) : (
+                        <video
+                          src={media.url}
+                          className={"w-full rounded-xl object-cover h-full"}
+                          controls
+                        />
+                      )}
+                      <button
+                        type="button"
+                        onClick={() => handleDeleteImage(index)}
+                        className="absolute left-2 top-2 bg-gray-900 hover:bg-gray-700 text-white rounded-full w-8 h-8 flex items-center justify-center"
+                      >
+                        <RemoveItemIcon className={"w-5 h-5"} />
+                      </button>
+                    </div>
+                  ))}
+                </div>
+
+
+
+                {isWritingTweet || tweet.length > 0 || mediaURLs.length > 0 ? (
                   <Composer.ChooseCanReply
                     whoCanReply={whoCanReply}
                     setCanReply={setCanReply}
                   />
-                ): (null)}
+                ) : null}
+
                 <div className="flex">
                   <div className="flex justify-between w-full my-3">
                     <div className="w-full flex">
 
-                      <button type="button" className="p-2 hover:bg-primary-extraLight w-fit rounded-full cursor-pointer">
-                        <label
-                          htmlFor="file-input"
-                          className="cursor-pointer w-8 h-8"
-                        >
-                          <ImageIcon
-                            className={
-                              "w-5 h-5 text-primary-base fill-current font-bold"
-                            }
-                          />
-                        </label>
+
+                      <label
+                        htmlFor="file-input"
+                        className={`w-fit p-2 ${
+                          mediaURLs.some((media) => media.type === "video") || mediaURLs.length >= 4
+                            ? "opacity-50"
+                            : "hover:bg-primary-extraLight rounded-full"
+                        } ${mediaURLs.every((media) => media.type === "image") && mediaURLs.length < 4 && "cursor-pointer"}`}
+                        aria-disabled={mediaURLs.some((media) => media.type === "video") || mediaURLs.length >= 4}
+                      >
+                        <span className="w-8 h-8">
+                          <ImageIcon className="w-5 h-5 text-primary-base font-bold" />
+                        </span>
                         <input
+                          disabled={mediaURLs.some((media) => media.type === "video") || mediaURLs.length >= 4}
                           className="hidden"
                           id="file-input"
                           type="file"
-                          onChange={handleFileUpload}
+                          multiple
+                          accept="image/*,video/*,.gif"
+                          onChange={handleImageChange}
                         />
-                      </button>
+                      </label>
 
-                      <button type="button" className="p-2 hover:bg-primary-extraLight w-fit rounded-full cursor-pointer">
-                        <label className="cursor-pointer w-8 h-8">
+
+
+                      <button
+                        type="button"
+                        disabled={mediaURLs.length > 0}
+                        className={`w-fit p-2 'hover:bg-primary-extraLight  rounded-full' ${
+                          mediaURLs.length > 0 && "opacity-50"
+                        }`}
+                      >
+                        <label
+                          className={`w-8 h-8 ${
+                            mediaURLs.length <= 0 && "cursor-pointer"
+                          }`}
+                        >
                           <GIFIcon
                             className={
                               "w-5 h-5 text-primary-base fill-current font-bold"
@@ -130,9 +220,19 @@ const TweetComposer = () => {
                           />
                         </label>
                       </button>
-                      
-                      <button type="button" className="p-2 hover:bg-primary-extraLight w-fit rounded-full cursor-pointer">
-                        <label className="cursor-pointer w-8 h-8">
+
+                      <button
+                        type="button"
+                        disabled={mediaURLs.length > 0}
+                        className={`w-fit p-2 'hover:bg-primary-extraLight  rounded-full' ${
+                          mediaURLs.length > 0 && "opacity-50"
+                        }`}
+                      >
+                        <label
+                          className={`w-8 h-8 ${
+                            mediaURLs.length <= 0 && "cursor-pointer"
+                          }`}
+                        >
                           <PollIcon
                             className={
                               "w-5 h-5 text-primary-base fill-current font-bold"
@@ -143,7 +243,10 @@ const TweetComposer = () => {
 
                       <Emoji setTweet={setTweet} />
 
-                      <button type="button" className="p-2 hover:bg-primary-extraLight w-fit rounded-full cursor-pointer">
+                      <button
+                        type="button"
+                        className="p-2 hover:bg-primary-extraLight w-fit rounded-full cursor-pointer"
+                      >
                         <label className="cursor-pointer w-8 h-8">
                           <ScheduleIcon
                             className={"w-5 h-5 text-primary-base fill-current"}
@@ -167,7 +270,9 @@ const TweetComposer = () => {
                             </div>
                           </>
                         )}
-                        <button disabled={tweet.length===0} type="submit"
+                        <button
+                          disabled={tweet.length === 0 && mediaURLs.length===0}
+                          type="submit"
                           className="border h-full px-3 ml-3 rounded-full bg-primary-base hover:bg-primary-dark text-white font-bold"
                         >
                           Tweet
--- src/icons/Icon.js
+++ src/icons/Icon.js
@@ -256,6 +256,16 @@ export const SelectedIcon = ({className}) => {
   )
 }
 
+export const RemoveItemIcon = ({className}) => {
+  return(
+    <Svg className={className}>
+      <g>
+        <path d="M10.59 12L4.54 5.96l1.42-1.42L12 10.59l6.04-6.05 1.42 1.42L13.41 12l6.05 6.04-1.42 1.42L12 13.41l-6.04 6.05-1.42-1.42L10.59 12z"></path>
+      </g>
+    </Svg>
+  )
+}
+
 export const PopulerIcon = ({ className }) => {
   return (
     <Svg className={className}>

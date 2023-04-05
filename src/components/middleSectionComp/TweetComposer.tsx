56bcf2f83b006ef466f2cecfd267ad8b17394e57
onur <onurabat3@gmail.com>
2023-04-06 01:48:39 +0300

    improvement: Media states updated

--- src/components/middleSectionComp/TweetComposer.tsx
+++ src/components/middleSectionComp/TweetComposer.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useCallback } from "react";
+import React, { useState, useRef, useCallback, useEffect } from "react";
 import {
   ImageIcon,
   PollIcon,
@@ -8,6 +8,7 @@ import {
 } from "@icons/Icon";
 import { Composer } from "@components/index";
 import GIFMenu from "./ComposerComp/GIFMenu";
+import { TenorImage } from "gif-picker-react";
 
 const TweetComposer = () => {
   const [isWritingTweet, setWiritinTweet] = useState(false);
@@ -16,9 +17,27 @@ const TweetComposer = () => {
   const [whoCanReply, setCanReply] = useState("Everyone");
 
   const [tweet, setTweet] = useState("");
-  const [mediaURLs, setMediaURLs] = useState<{ url: string; type: "image" | "video" | "gif";}[]>([]);
+  const [mediaFiles, setMediaFiles] = useState<Array<{file: File, url: string, type: string}>>([]);
+  const [mediaAveilable, setMediaAveilable] = useState(true)
 
-  console.log(mediaURLs)
+  const [tenorGif, setTenorGif] = useState<TenorImage | undefined>()
+  const [gifAvailable, setGifAveilable] = useState(true)
+
+
+  useEffect(()=> {
+    (mediaFiles.length>0 || tenorGif) ? setGifAveilable(false) : setGifAveilable(true);
+    (mediaFiles.length>=4 || tenorGif) ? setMediaAveilable(false) : setMediaAveilable(true);
+
+    mediaFiles.map(media => {
+      if(media.type === "video") {
+        setMediaAveilable(false)
+      }
+    })
+
+  }, [mediaFiles, tenorGif])
+
+  console.log("gifAvailable", gifAvailable);
+  console.log("mediaAveilable", mediaAveilable);
 
 
   const textAreaRef = useRef<HTMLTextAreaElement>(null);
@@ -38,13 +57,14 @@ const TweetComposer = () => {
     if (!files) return;
   
     const mediaFiles = Array.from(files).map((file) => ({
+      file,
       url: URL.createObjectURL(file),
       type: file.type.startsWith("image") ? "image" : "video",
     }));
   
-    setMediaURLs((prevMediaURLs: any) => {
-      const imagesCount = prevMediaURLs.filter((media: any) => media.type === "image").length;
-      const videoCount = prevMediaURLs.filter((media: any) => media.type === "video").length;
+    setMediaFiles((prevMediaFiles) => {
+      const imagesCount = prevMediaFiles.filter((media) => media.type === "image").length;
+      const videoCount = prevMediaFiles.filter((media) => media.type === "video").length;
   
       const newImages = mediaFiles.filter((media) => media.type === "image");
       const newVideo = mediaFiles.find((media) => media.type === "video");
@@ -57,29 +77,38 @@ const TweetComposer = () => {
       }
   
       if (videoCount === 0 && newImages.length > 0) {
-        return [...prevMediaURLs, ...imagesToAdd];
+        return [...prevMediaFiles, ...imagesToAdd];
       }
   
-      return prevMediaURLs;
+      return prevMediaFiles;
     });
   };
   
   
+  
   const handleDeleteImage = (indexToDelete: number) => {
-    setMediaURLs((prevmediaURLs) =>
+    setMediaFiles((prevmediaURLs) =>
       prevmediaURLs.filter((_, index) => index !== indexToDelete)
     );
   };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
+  
     const formData = new FormData();
     formData.append("tweet", tweet);
-    formData.append("audience", Audience);  
+    formData.append("audience", Audience);
     formData.append("whoCanReply", whoCanReply);
-
-    
+  
+    // Append media files if they exist
+    if (mediaFiles.length > 0) {
+      mediaFiles.forEach((media) => {
+        formData.append("mediaFiles", media.file);
+      });
+    } else if (tenorGif) { // If no media files, check for tenorGif
+      // Assuming TenorImage has a 'url' property
+      formData.append("tenorGif", JSON.stringify(tenorGif));
+    }
   };
 
   return (
@@ -98,7 +127,7 @@ const TweetComposer = () => {
             </div>
             <div className="flex flex-col w-full pt-1">
               <form onSubmit={handleSubmit} className="flex flex-col">
-                {isWritingTweet || tweet.length > 0 || mediaURLs.length > 0 ? (
+                {isWritingTweet || tweet.length > 0 || mediaFiles.length > 0 ? (
                   <Composer.ChooseAudience
                     Audience={Audience}
                     setAudience={setAudience}
@@ -124,42 +153,61 @@ const TweetComposer = () => {
 
 
                 <div
-                    className={`grid gap-2 mb-1 ${
-                      mediaURLs.length === 1 || (mediaURLs.length === 2 && mediaURLs[0].type === "video")
-                        ? "grid-cols-1"
-                        : "grid-cols-2"
-                    }`}
-                  >
-                  {mediaURLs.map((media, index) => (
-                    <div key={index} className="relative">
-                      {media.type === "image" || "gif" ? (
+                  className={`grid gap-2 mb-1 ${
+                    mediaFiles.length === 1 || (mediaFiles.length === 2 && mediaFiles[0].type === "video") || tenorGif
+                      ? "grid-cols-1"
+                      : "grid-cols-2"
+                  }`}
+                >
+                  {mediaFiles.length>0 ? (
+                    mediaFiles.map((media, index) => (
+                      <div key={index} className="relative">
+                        {media.type === "image" || media.type ==="gif" ? (
+                          <img
+                            src={media.url}
+                            alt={`Selected file ${index + 1}`}
+                            className={`w-full rounded-xl object-cover ${
+                              mediaFiles.length > 1 ? "h-36" : "h-full"
+                            }`}
+                          />
+                        ) : (
+                          <video
+                            src={media.url}
+                            className={"w-full rounded-xl object-cover h-full"}
+                            controls
+                          />
+                        )}
+                        <button
+                          type="button"
+                          onClick={() => handleDeleteImage(index)}
+                          className="absolute left-2 top-2 bg-gray-900 hover:bg-gray-700 text-white rounded-full w-8 h-8 flex items-center justify-center"
+                        >
+                          <RemoveItemIcon className={"w-5 h-5"} />
+                        </button>
+                      </div>
+                    ))
+                  ) : tenorGif && (
+                      <div className="relative">
                         <img
-                          src={media.url}
-                          alt={`Selected file ${index + 1}`}
-                          className={`w-full rounded-xl object-cover ${
-                            mediaURLs.length > 1 ? "h-36" : "h-full"
-                          }`}
+                          src={tenorGif?.preview.url}
+                          alt={`${tenorGif?.description}`}
+                          className="w-full rounded-xl object-cover h-full"
                         />
-                      ) : (
-                        <video
-                          src={media.url}
-                          className={"w-full rounded-xl object-cover h-full"}
-                          controls
-                        />
-                      )}
-                      <button
-                        type="button"
-                        onClick={() => handleDeleteImage(index)}
-                        className="absolute left-2 top-2 bg-gray-900 hover:bg-gray-700 text-white rounded-full w-8 h-8 flex items-center justify-center"
-                      >
-                        <RemoveItemIcon className={"w-5 h-5"} />
-                      </button>
-                    </div>
-                  ))}
+          
+                        <button
+                          type="button"
+                          onClick={() => setTenorGif(undefined)}
+                          className="absolute left-2 top-2 bg-gray-900 hover:bg-gray-700 text-white rounded-full w-8 h-8 flex items-center justify-center"
+                        >
+                          <RemoveItemIcon className={"w-5 h-5"} />
+                        </button>
+                      </div>
+                  )}
                 </div>
 
 
-                {isWritingTweet || tweet.length > 0 || mediaURLs.length > 0 ? (
+
+                {isWritingTweet || tweet.length > 0 || mediaFiles.length > 0 ? (
                   <Composer.ChooseCanReply
                     whoCanReply={whoCanReply}
                     setCanReply={setCanReply}
@@ -172,18 +220,13 @@ const TweetComposer = () => {
 
                       <label
                         htmlFor="file-input"
-                        className={`w-fit p-2 ${
-                          mediaURLs.some((media) => media.type === "video" || media.type === "gif") || mediaURLs.length >= 4
-                            ? "opacity-50"
-                            : "hover:bg-primary-extraLight rounded-full"
-                        } ${mediaURLs.every((media) => media.type === "image") && mediaURLs.length < 4 && "cursor-pointer"}`}
-                        aria-disabled={mediaURLs.some((media) => media.type === "video" || media.type === "gif") || mediaURLs.length >= 4}
+                        className={`w-fit p-2 ${mediaAveilable ? "hover:bg-primary-extraLight rounded-full cursor-pointer" : "opacity-50"}`}
                       >
                         <span className="w-8 h-8">
                           <ImageIcon className="w-5 h-5 text-primary-base font-bold" />
                         </span>
                         <input
-                          disabled={mediaURLs.some((media) => media.type === "video" || media.type === "gif") || mediaURLs.length >= 4}
+                          disabled={!mediaAveilable}
                           className="hidden"
                           id="file-input"
                           type="file"
@@ -193,19 +236,21 @@ const TweetComposer = () => {
                         />
                       </label>
 
-                      <GIFMenu setMediaURLs={setMediaURLs}  mediaURLs={mediaURLs} />
+                      <GIFMenu 
+                        tenorGif={tenorGif}
+                        setTenorGif={setTenorGif}
+                        gifAvailable={gifAvailable}
+                      />
 
 
                       <button
                         type="button"
-                        disabled={mediaURLs.length > 0}
-                        className={`w-fit p-2 'hover:bg-primary-extraLight  rounded-full' ${
-                          mediaURLs.length > 0 && "opacity-50"
-                        }`}
+                        disabled={!gifAvailable || !mediaAveilable}
+                        className={`w-fit p-2 ${mediaFiles.length>0 || !gifAvailable ? "opacity-50" : "hover:bg-primary-extraLight rounded-full"}`}
                       >
                         <label
                           className={`w-8 h-8 ${
-                            mediaURLs.length <= 0 && "cursor-pointer"
+                            (mediaFiles.length===0 && gifAvailable ?  "cursor-pointer" : (""))
                           }`}
                         >
                           <PollIcon
@@ -246,7 +291,7 @@ const TweetComposer = () => {
                           </>
                         )}
                         <button
-                          disabled={tweet.length === 0 && mediaURLs.length===0}
+                          disabled={tweet.length === 0 && mediaFiles.length===0}
                           type="submit"
                           className="border h-full px-3 ml-3 rounded-full bg-primary-base hover:bg-primary-dark text-white font-bold"
                         >

d5306791274a390a93fe4e4a57dd71d8d2ecdc20
onur <onurabat3@gmail.com>
2023-05-19 23:05:07 +0300

    feat: added media and likes section in user profile feed.

--- src/components/middleSectionComp/UserProfile/UserFeed/MediaTab.tsx
+++ src/components/middleSectionComp/UserProfile/UserFeed/MediaTab.tsx
@@ -1,7 +1,6 @@
-import React from "react";
 import { useQuery } from "@tanstack/react-query";
-import { getUserTweets } from "api/userApi";
 import { ITweet } from "@customTypes/TweetTypes";
+import { getMediaOnlyUserTweets } from "api/userApi";
 import { LoadingIcon, RetryIcon } from "@icons/Icon";
 import TweetCard from "@components/middleSectionComp/TweetCard";
 
@@ -9,15 +8,15 @@ interface IProps {
   username: string;
 }
 
-const TweetsTab = ({ username }: IProps) => {
-  const userTweetsQuery = useQuery<ITweet[]>({
-    queryKey: ["userTweets", username],
-    queryFn: () => getUserTweets(username),
+const MediaTab = ({ username }: IProps) => {
+  const mediaOnlyUserTweetsQuery = useQuery<ITweet[]>({
+    queryKey: ["mediaOnlyUserTweets", username],
+    queryFn: () => getMediaOnlyUserTweets(username),
     retry: false,
     refetchOnWindowFocus: false,
   });
 
-  if (userTweetsQuery.isLoading) {
+  if (mediaOnlyUserTweetsQuery.isLoading) {
     return (
       <div className="flex p-8 justify-center">
         <LoadingIcon />
@@ -25,14 +24,14 @@ const TweetsTab = ({ username }: IProps) => {
     );
   }
 
-  if (userTweetsQuery.isError) {
+  if (mediaOnlyUserTweetsQuery.isError) {
     return (
       <div className="flex flex-col max-w-600px w-full justify-center items-center py-5 px-3">
         <span className="mb-5 text-center">
           Something went wrong. Try reloading.
         </span>
         <button
-          onClick={() => userTweetsQuery.refetch()}
+          onClick={() => mediaOnlyUserTweetsQuery.refetch()}
           className="flex gap-1 items-center px-4 py-2 min-h-[36px] bg-primary-base hover:bg-primary-dark duration-200 rounded-full"
         >
           <RetryIcon className="w-6 h-6 text-white" />
@@ -41,11 +40,11 @@ const TweetsTab = ({ username }: IProps) => {
       </div>
     );
   }
-
-  if (userTweetsQuery.data.length > 0) {
+  
+  if (mediaOnlyUserTweetsQuery.data.length > 0) {
     return (
       <div className="flex flex-col">
-        {userTweetsQuery.data.map((tweet, index) => (
+        {mediaOnlyUserTweetsQuery.data.map((tweet, index) => (
           <div key={index} className="border-b">
             <TweetCard
               key={index}
@@ -62,11 +61,17 @@ const TweetsTab = ({ username }: IProps) => {
   return (
     <div className="flex p-8 justify-center">
       <div className="flex flex-col max-w-sm">
-        <span className="text-3xl font-bold">@{username} hasn't tweeted</span>
+        <img
+          src="https://res.cloudinary.com/dwcw9iftp/image/upload/v1684520718/Twitter/Tweets/masked-doll-head-with-camera-800x400.png"
+          alt=""
+        />
+        <span className="text-3xl font-bold">
+          @{username} hasn't Tweeted Media
+        </span>
         <span>When they do, their Tweets will show up here.</span>
       </div>
     </div>
   );
 };
 
-export default React.memo(TweetsTab);
+export default MediaTab;
